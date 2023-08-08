ded37749469639912c822c0eaf01a94a52335a59
Luisa Linhares <luisalaura39@gmail.com>
2023-08-08 00:28:08 -0300

    Atualizando script de banco

--- sql_criar_tabelas/1-CriarBanco.sql
+++ sql_criar_tabelas/1-CriarBanco.sql
@@ -6,6 +6,11 @@ CREATE TABLE "Usuario" (
   "DadosBancario" varchar(255)
 );
 
+CREATE TABLE "GeneroMusical" (
+  "GeneroMusical_PK" int PRIMARY KEY,
+  "GeneroMusical" varchar(255)
+);
+
 CREATE TABLE "Produto" (
   "CodigoBarras" varchar(255) PRIMARY KEY,
   "Nome" varchar(255),
@@ -15,16 +20,15 @@ CREATE TABLE "Produto" (
   "DataLancamento" Date,
   "fk_Usuario_Cnpj" varchar(255),
   FOREIGN KEY ("fk_Usuario_Cnpj") REFERENCES "Usuario" ("CNPJ")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE "Album" (
   "fk_Produto_CodigoBarras" varchar(255) PRIMARY KEY,
   CONSTRAINT "fk_Album_fk_Produto_CodigoBarras" FOREIGN KEY ("fk_Produto_CodigoBarras") REFERENCES "Produto" ("CodigoBarras")
-);
-
-CREATE TABLE "GeneroMusical" (
-  "GeneroMusical_PK" int PRIMARY KEY,
-  "GeneroMusical" varchar(255)
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE "FaixaMusical_Single" (
@@ -34,10 +38,16 @@ CREATE TABLE "FaixaMusical_Single" (
   "fk_GeneroMusical_GeneroMusical_PK" int,
   "FaixaMusical" varchar(255),
   "fk_Album_fk_Produto_CodigoBarras" varchar(255) UNIQUE,
-  "fk_Produto_CodigoBarras" varchar(255) UNIQUE,  -- Adiciona a restrição UNIQUE nesta coluna
-  FOREIGN KEY ("fk_GeneroMusical_GeneroMusical_PK") REFERENCES "GeneroMusical" ("GeneroMusical_PK"),
-  FOREIGN KEY ("fk_Album_fk_Produto_CodigoBarras") REFERENCES "Album" ("fk_Produto_CodigoBarras"),
+  "fk_Produto_CodigoBarras" varchar(255) UNIQUE,
+  FOREIGN KEY ("fk_GeneroMusical_GeneroMusical_PK") REFERENCES "GeneroMusical" ("GeneroMusical_PK")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
+  FOREIGN KEY ("fk_Album_fk_Produto_CodigoBarras") REFERENCES "Album" ("fk_Produto_CodigoBarras")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
   FOREIGN KEY ("fk_Produto_CodigoBarras") REFERENCES "Produto" ("CodigoBarras")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE "Pessoa" (
@@ -47,6 +57,8 @@ CREATE TABLE "Pessoa" (
   "CPF" char(11) PRIMARY KEY,
   "fk_Usuario_CNPJ" char(14),
   FOREIGN KEY ("fk_Usuario_CNPJ") REFERENCES "Usuario" ("CNPJ")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE "Participa" (
@@ -55,22 +67,33 @@ CREATE TABLE "Participa" (
   "fk_FaixaMusical_Single_ISRC" varchar(255),
   "fk_FaixaMusical_Single_fk_Produto_CodigoBarras" varchar(255),
   "TipoPessoa" varchar(255),
-  FOREIGN KEY ("fk_Pessoa_CPF") REFERENCES "Pessoa" ("CPF"),
-  FOREIGN KEY ("fk_FaixaMusical_Single_ISRC") REFERENCES "FaixaMusical_Single" ("ISRC"),
+  FOREIGN KEY ("fk_Pessoa_CPF") REFERENCES "Pessoa" ("CPF")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
+  FOREIGN KEY ("fk_FaixaMusical_Single_ISRC") REFERENCES "FaixaMusical_Single" ("ISRC")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
   FOREIGN KEY ("fk_FaixaMusical_Single_fk_Produto_CodigoBarras") REFERENCES "FaixaMusical_Single" ("fk_Produto_CodigoBarras")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE "PlataformaDigital" (
   "CNPJ" char(14) PRIMARY KEY,
-  "Nome" varchar(255)
+  "Nome" varchar(255),
+  "ValorPagoReproducao" float
 );
 
 CREATE TABLE "Distribuido" (
   "fk_PlataformaDigital_cnpj" char(14),
   "fk_Produto_CodigoBarras" varchar(255),
   PRIMARY KEY ("fk_PlataformaDigital_cnpj", "fk_Produto_CodigoBarras"),
-  FOREIGN KEY ("fk_PlataformaDigital_cnpj") REFERENCES "PlataformaDigital" ("CNPJ"),
+  FOREIGN KEY ("fk_PlataformaDigital_cnpj") REFERENCES "PlataformaDigital" ("CNPJ")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
   FOREIGN KEY ("fk_Produto_CodigoBarras") REFERENCES "Produto" ("CodigoBarras")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
 
 CREATE TABLE "Reproduzida" (
@@ -80,44 +103,13 @@ CREATE TABLE "Reproduzida" (
   "fk_FaixaMusical_Single_fk_Produto_CodigoBarras" varchar(255),
   "DataReproducao" Date,
   "QuantidadeReproducao" int,
-  FOREIGN KEY ("fk_PlataformaDigital_cnpj") REFERENCES "PlataformaDigital" ("CNPJ"),
-  FOREIGN KEY ("fk_FaixaMusical_Single_ISRC") REFERENCES "FaixaMusical_Single" ("ISRC"),
+  FOREIGN KEY ("fk_PlataformaDigital_cnpj") REFERENCES "PlataformaDigital" ("CNPJ")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
+  FOREIGN KEY ("fk_FaixaMusical_Single_ISRC") REFERENCES "FaixaMusical_Single" ("ISRC")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE,
   FOREIGN KEY ("fk_FaixaMusical_Single_fk_Produto_CodigoBarras") REFERENCES "FaixaMusical_Single" ("fk_Produto_CodigoBarras")
+    ON DELETE SET NULL
+    ON UPDATE CASCADE
 );
-
-INSERT INTO "PlataformaDigital" ("CNPJ", "Nome") VALUES
-  ('11111111111111', 'Spotify'),
-  ('22222222222222', 'Apple Music'),
-  ('33333333333333', 'Deezer'),
-  ('44444444444444', 'Amazon Music'),
-  ('55555555555555', 'YouTube Music');
- 
- INSERT INTO "PlataformaDigital" ("CNPJ", "Nome") VALUES
-  ('66666666666666', 'Tidal'),
-  ('77777777777777', 'SoundCloud'),
-  ('88888888888888', 'Pandora'),
-  ('99999999999999', 'Google Play Music');
-
-  
- 
- INSERT INTO "GeneroMusical" ("GeneroMusical_PK", "GeneroMusical") VALUES
-  (1, 'Pop'),
-  (2, 'Rock'),
-  (3, 'Hip Hop'),
-  (4, 'Eletrônica'),
-  (5, 'Sertanejo');
-
- INSERT INTO "GeneroMusical" ("GeneroMusical_PK", "GeneroMusical") VALUES
-  (6, 'R&B'),
-  (7, 'Indie'),
-  (8, 'Reggae'),
-  (9, 'Funk'),
-  (10, 'Country');
-
-  INSERT INTO "GeneroMusical" ("GeneroMusical_PK", "GeneroMusical") VALUES
-  (16, 'Pop Rock'),
-  (17, 'Rap'),
-  (18, 'Electronic Dance Music'),
-  (19, 'Country Pop'),
-  (20, 'Reggaeton');
-
---
+++ sql_criar_tabelas/2-Inserir.sql
@@ -0,0 +1,35 @@
+INSERT INTO "PlataformaDigital" ("CNPJ", "Nome", "ValorPagoReproducao") VALUES
+  ('11111111111111', 'Spotify', 0.00348),
+  ('22222222222222', 'Apple Music', 0.00675),
+  ('33333333333333', 'Deezer', 0.00562),
+  ('44444444444444', 'Amazon Music', 0.01123),
+  ('55555555555555', 'YouTube Music', 0.00022);
+ 
+INSERT INTO "PlataformaDigital" ("CNPJ", "Nome", "ValorPagoReproducao") VALUES
+  ('66666666666666', 'Tidal', 0.00876),
+  ('77777777777777', 'SoundCloud', 0.00252),
+  ('88888888888888', 'Pandora', 0.00203),
+  ('99999999999999', 'Google Play Music', 0.00475);
+
+  
+ 
+ INSERT INTO "GeneroMusical" ("GeneroMusical_PK", "GeneroMusical") VALUES
+  (1, 'Pop'),
+  (2, 'Rock'),
+  (3, 'Hip Hop'),
+  (4, 'Eletrônica'),
+  (5, 'Sertanejo');
+
+ INSERT INTO "GeneroMusical" ("GeneroMusical_PK", "GeneroMusical") VALUES
+  (6, 'R&B'),
+  (7, 'Indie'),
+  (8, 'Reggae'),
+  (9, 'Funk'),
+  (10, 'Country');
+
+  INSERT INTO "GeneroMusical" ("GeneroMusical_PK", "GeneroMusical") VALUES
+  (16, 'Pop Rock'),
+  (17, 'Rap'),
+  (18, 'Electronic Dance Music'),
+  (19, 'Country Pop'),
+  (20, 'Reggaeton');
